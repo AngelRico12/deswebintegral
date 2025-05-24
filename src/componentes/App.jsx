90cfeecff40c135cd0ce8bfdd86105024426acc5
AngelRico12 <angelrico122001@gmail.com>
2025-05-23 22:57:10 -0600

    Versión 1

---
+++ src/componentes/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './NavBar'; // Asegúrate de que el archivo se llama Navbar.jsx y esté en el mismo nivel
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>Bienvenido a mi página Web</h1>
+        <p>Esta es solo práctica de desarrollo web integral</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
